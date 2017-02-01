c03c0c8ecee9cfab82075f9c1e5d37ee41056b02
alvinlyj <alvin_loh@u.nus.edu>
2017-02-01 18:21:01 +0800

    Corrected code for handling file not found exception

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -117,7 +117,7 @@ public class StorageFile {
      *
      * @throws StorageOperationException if there were errors reading and/or converting data from file.
      */
-    public AddressBook load() throws StorageOperationException, FileNotFoundException {
+    public AddressBook load() throws StorageOperationException {
         try (final Reader fileReader =
                      new BufferedReader(new FileReader(path.toFile()))) {
 
